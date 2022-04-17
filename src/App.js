8924c6b715b8d0df3bb04d81a13f107cc80ea3b0
wahyu <wahyu@NSJR.local>
2022-04-17 21:39:11 +0700

    add login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import logo from './asset/Frame.png';
 import './App.css';
+import './bootstrap.css'
+import { Row, Col, Container, Button, Form } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Container>
+  <Row>
+    <Col>
+    <div className="left">
+      <img className="logo ms-1" src={logo} />
+      <h2 className="Text mt-3">Easy, Fast, and Relieble</h2>
+      <h5 className="Text-detail">Go Shopping for Merchandise, just go to dumb merch shopping. The biggest merchandise in Indonesia</h5>
+      <div className='button-group'>
+      <a className='login' type='button'>Log In</a>{' '}
+      <Button className='button-register' variant="secondary">Register</Button>{' '}
+      </div>
+    </div>
+      </Col>
+    <Col>
+    <div className='right-side'>
+    <div className='right'>
+    <h3 className="Text">Login</h3>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <input className='inputEmail col-12' type="email" placeholder="Enter email" />
+
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <input className='inputEmail col-12' type="password" placeholder="Password" />
+      </Form.Group>
+
+      <a className="loginform col-12 mt-4" type="submit">
+        Submit
+      </a>
+</Form>
+    </div>
+    </div>
+    </Col>
+  </Row>
+</Container>
+
     </div>
   );
 }
